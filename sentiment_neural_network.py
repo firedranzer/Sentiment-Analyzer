4239fbe171229d0c20bd087ef7d849714fedab74
firedranzer <rupeshharode@gmail.com>
2017-11-02 23:26:35 +0530

    First Commit

---
+++ sentiment_neural_network.py
@@ -0,0 +1,77 @@
+import tensorflow as tf
+import numpy as np
+from create_sentiment import create_featureset_and_return_labels
+
+train_x, train_y, test_x, test_y = create_featureset_and_return_labels('pos.txt', 'neg.txt')
+
+n_nodes_hl1 = 1500
+n_nodes_hl2 = 1500
+n_nodes_hl3 = 1500
+
+n_classes = 2
+batch_size = 100
+
+x = tf.placeholder('float')
+y = tf.placeholder('float')
+
+
+def neural_network_model(data):
+
+    hidden_layer_1 = {'weights': tf.Variable(tf.random_normal([len(train_x[0]), n_nodes_hl1])),
+                      'biases': tf.Variable(tf.random_normal([n_nodes_hl1]))}
+    hidden_layer_2 = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+                      'biases': tf.Variable(tf.random_normal([n_nodes_hl2]))}
+    hidden_layer_3 = {'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
+                      'biases': tf.Variable(tf.random_normal([n_nodes_hl3]))}
+    output_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
+                    'biases': tf.Variable(tf.random_normal([n_classes]))}
+
+    l1 = tf.add(tf.matmul(data, hidden_layer_1['weights']), hidden_layer_1['biases'])
+    l1 = tf.nn.relu(l1)
+
+    l2 = tf.add(tf.matmul(l1, hidden_layer_2['weights']), hidden_layer_2['biases'])
+    l2 = tf.nn.relu(l2)
+
+    l3 = tf.add(tf.matmul(l2, hidden_layer_3['weights']), hidden_layer_3['biases'])
+    l3 = tf.nn.relu(l3)
+
+    output = tf.matmul(l3, output_layer['weights']) + output_layer['biases']
+
+    return output
+
+
+def train_neural_network(data):
+
+    prediction = neural_network_model(x)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=prediction))
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        hm_epoch = 10
+
+        for epoch in range(hm_epoch):
+
+            epoch_loss = 0
+
+            i = 0
+            while i < len(train_x):
+                start = i
+                end = i + batch_size
+
+                batch_x = np.array(train_x[start:end])
+                batch_y = np.array(train_y[start:end])
+
+                _, c = sess.run([optimizer, cost], feed_dict = {x: batch_x, y: batch_y})
+                epoch_loss += c
+                i += batch_size
+
+            print('Epoch', epoch, 'completed of', hm_epoch, 'loss: ', epoch_loss)
+
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        print('Accuracy:', accuracy.eval({x: test_x, y: test_y}))
+
+
+train_neural_network(x)
